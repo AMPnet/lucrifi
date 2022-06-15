712d08989f82c9acf4de3bb41924683a3ca59d54
Bartol Freskura <bartol.freskura@velebit.ai>
2022-06-15 16:01:29 +0200

    add loading spinner while connecting wallet

--- components/Authorization/SignIn.vue
+++ components/Authorization/SignIn.vue
@@ -30,7 +30,24 @@ async function connectWallet() {
           v-else
           class="mt-3 w-full font-bold text-xs rounded-full bg-gradient-to-r from-sky-600 to-cyan-500 py-3 px-6 text-white"
         >
-          Connecting...
+          <div class="flex justify-center items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5 animate-spin mr-1.5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+              />
+            </svg>
+
+            <span> Please wait</span>
+          </div>
         </button>
       </div>
     </div>
