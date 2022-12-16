c64778a0c921f4bc9b59571abd56a90e78a3f8da
Bartol Freskura <bartol.freskura@velebit.ai>
2022-12-16 14:33:18 +0100

    Add success message on email submit

--- components/PromoBanner.vue
+++ components/PromoBanner.vue
@@ -23,6 +23,7 @@ const showBanner = computed(
 
 const email = ref("");
 const errorMessage = ref("");
+const successMessage = ref("");
 
 async function submitMail() {
   errorMessage.value = "";
@@ -71,36 +72,69 @@ async function submitMail() {
   };
   addressBook.addToAddressBook(alias);
   email.value = "";
+
+  successMessage.value =
+    "We have received your email. Enjoy the Unlimited LucriFi Subscription!";
+  setTimeout(() => {
+    successMessage.value = "";
+  }, 10000);
 }
 </script>
 
 <template>
   <div>
     <div
-      v-if="showBanner"
-      class="bg-gradient-to-r from-teal-400 to-purple-600 px-8 py-2 rounded-xl lg:rounded-full flex items-center text-sm justify-center gap-y-4 gap-x-6 text-white font-semibold flex-wrap mx-3"
+      v-if="showBanner || successMessage.length > 0"
+      class="bg-gradient-to-r from-teal-400 to-purple-600 px-8 py-2 rounded-xl lg:rounded-full text-sm justify-center text-white font-semibold mx-3"
     >
-      <div class="bg-slate-700 rounded-md py-2 px-3 text-xs">
-        HOLIDAYS PROMO
-      </div>
-      <div class="text-center">Get UNLIMITED Subscription for FREE</div>
-      <div>
-        <input
-          class="px-3 py-2 w-56 rounded-md text-slate-800 text-xs focus:outline-none"
-          placeholder="Your Email Address"
-          type="email"
-          v-model="email"
-        />
-      </div>
-      <button
-        :disabled="!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)"
-        @click="submitMail"
-        class="rounded-full bg-violet-700 px-3 py-2 text-xs hover:bg-violet-600 disabled:opacity-50"
+      <div
+        v-if="successMessage.length > 0"
+        class="flex items-center gap-x-2 justify-center py-1"
       >
-        Get UNLIMITED
-      </button>
-      <div class="text-amber-400 text-xs" v-if="errorMessage.length > 0">
-        {{ errorMessage }}
+        <div>{{ successMessage }} fjeiofjeoijfoij</div>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </div>
+      </div>
+      <div
+        v-else
+        class="gap-x-6 gap-y-4 flex items-center justify-center flex-wrap"
+      >
+        <div class="bg-slate-700 rounded-md py-2 px-3 text-xs flex-wrap">
+          HOLIDAYS PROMO
+        </div>
+        <div class="text-center">Get UNLIMITED Subscription for FREE</div>
+        <div>
+          <input
+            class="px-3 py-2 w-56 rounded-md text-slate-800 text-xs focus:outline-none"
+            placeholder="Your Email Address"
+            type="email"
+            v-model="email"
+          />
+        </div>
+        <button
+          :disabled="!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)"
+          @click="submitMail"
+          class="rounded-full bg-violet-700 px-3 py-2 text-xs hover:bg-violet-600 disabled:opacity-50"
+        >
+          Get UNLIMITED
+        </button>
+        <div class="text-amber-400 text-xs" v-if="errorMessage.length > 0">
+          {{ errorMessage }}
+        </div>
       </div>
     </div>
   </div>
